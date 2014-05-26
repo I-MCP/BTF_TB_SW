c6b25ea0089bb567c6894c2322b79359e5b4cbe8
Paolo <paolo.meridiani@cern.ch>
2014-05-26 12:21:10 +0200

    Adding LinkDef

---
+++ include/BTFTBSW_LinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class iMCP_BTF_simpleRecoTree+;
+#pragma link C++ class iMCP_BTF_simpleRecoTree_format+;
+#pragma link C++ class Waveform+;
+#pragma link C++ class iMCP_BTF_rawDataTree+;
+#endif
