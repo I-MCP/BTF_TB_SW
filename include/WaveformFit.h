838b3b742890c75790dea1abc414bd624c5581ce
Paolo <paolo.meridiani@cern.ch>
2014-07-12 14:27:58 +0200

    Compiling version of shape fit

--- include/WaveformFit.h
+++ include/WaveformFit.h
@@ -2,10 +2,12 @@
 #define WAVEFORM_FIT_H
 
 #include "TProfile.h"
+#include "Waveform.h"
 #include "Math/Minimizer.h"
 
 namespace WaveformFit
 {
   void alignWaveform(TProfile* ref_profile, TProfile* fit_profile, ROOT::Math::Minimizer* &minimizer);
+  void fitWaveform(Waveform* wave, TProfile* amplitudeProfile, int x1, int x2, ROOT::Math::Minimizer* &minimizer);
 }
 #endif
--- src/WaveformFit.cc
+++ src/WaveformFit.cc
@@ -13,7 +13,8 @@ namespace WaveformFit
 {
   TProfile* refWave;
   TProfile* fitWave;
-
+  Waveform* waveData;
+  
   //   ROOT::Math::Minimizer* min =0;
 
   ROOT::Math::Interpolator* y_interpolator=0;
@@ -28,10 +29,23 @@ namespace WaveformFit
   {
     double chisq = 0;
     double delta = 0;
-    for (int i=1;i<refWave->GetNbinsX();++i)
+    for (int i=xMin;i<=xMax;++i)
       {
 	//	delta=(refWave->GetBinContent(i)-(y_interpolator->Eval(refWave->GetBinCenter(i)+par[1])+par[0]))/(TMath::Sqrt(refWave->GetBinError(i)*refWave->GetBinError(i)+y_err_interpolator->Eval(refWave->GetBinCenter(i)+par[1])*y_err_interpolator->Eval(refWave->GetBinCenter(i)+par[1])));
 	delta= (refWave->GetBinContent(i)-(y_interpolator->Eval(refWave->GetBinCenter(i)+par[1])+par[0])); //using no error for the moment in the Chi2 definition
+	chisq += delta*delta;
+      }
+    return chisq;
+  }
+
+  double chi2Wave(const double *par )
+  {
+    double chisq = 0;
+    double delta = 0;
+    for (int i=xMin;i<=xMax;++i)
+      {
+	//	delta=(refWave->GetBinContent(i)-(y_interpolator->Eval(refWave->GetBinCenter(i)+par[1])+par[0]))/(TMath::Sqrt(refWave->GetBinError(i)*refWave->GetBinError(i)+y_err_interpolator->Eval(refWave->GetBinCenter(i)+par[1])*y_err_interpolator->Eval(refWave->GetBinCenter(i)+par[1])));
+	delta= ((*waveData)._samples[i]-(y_interpolator->Eval((*waveData)._times[i]-par[1])*par[0])); //fit par[0]*ref_shape(t-par[1]) par[0]=amplitude, par[1]=DeltaT
 	chisq += delta*delta;
       }
     return chisq;
@@ -78,9 +92,55 @@ namespace WaveformFit
    
     const double* par=minimizer->X();
     std::cout << "+++++ FIT RESULT: " << par[0] << "," << par[1] << std::endl;
+    delete y_interpolator;
+    delete y_err_interpolator;
+    y_interpolator=0;
+    y_err_interpolator=0;
     //     minimizer=min;
   }
 
+  void fitWaveform(Waveform* wave, TProfile* amplitudeProfile, int x1, int x2, ROOT::Math::Minimizer* &minimizer)
+  {
+    xMin=x1;
+    xMax=x2;
+
+    waveData=wave;
+    fitWave=amplitudeProfile;
+
+    minimizer = ROOT::Math::Factory::CreateMinimizer("Minuit2", "Migrad");
+
+    minimizer->SetMaxFunctionCalls(1000000);
+    minimizer->SetMaxIterations(100000);
+    minimizer->SetTolerance(1e-6);
+    minimizer->SetPrintLevel(2);
+
+    if (!y_interpolator)
+      y_interpolator=new ROOT::Math::Interpolator(MAX_INTERPOLATOR_POINTS, ROOT::Math::Interpolation::kCSPLINE);
+
+    std::vector<double> x,y;
+
+    for (int i=1;i<=amplitudeProfile->GetNbinsX();++i)
+      {
+	x.push_back(amplitudeProfile->GetBinCenter(i));
+	y.push_back(amplitudeProfile->GetBinContent(i));
+      }
+
+    y_interpolator->SetData(x,y);
+
+    ROOT::Math::Functor f(&chi2Wave,2);
+    minimizer->SetFunction(f);
+
+    minimizer->SetVariable(0,"amplitude",100,1e-2);
+    minimizer->SetVariable(1,"deltaT",30.,1e-1);
+    minimizer->Minimize();
+   
+    const double* par=minimizer->X();
+
+    std::cout << "+++++ FIT RESULT: " << par[0] << "," << par[1] << std::endl;
+
+    delete y_interpolator;
+    y_interpolator=0;
+  }
 
   
 };
